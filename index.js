df042049ec94da0d8590513f64ce57bd7d0b1b4d
jussila <jussi.laine@solita.fi>
2024-09-04 17:03:42 +0300

    osa 3: Tehtävä 3.9 backend-osuus

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 morgan.token('body', function (req) {
@@ -108,7 +110,7 @@ app.post('/api/persons', (request, response) => {
   response.json(newPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
