65d895739aa9c42aff337a618e93b24d71041038
jussila <jussi.laine@solita.fi>
2024-09-20 10:28:15 +0300

    osa 3: Tehtävä 3.21 - bugikorjauksia validointiin

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, targetPerson, { new: true })
+  Person.findByIdAndUpdate(request.params.id, targetPerson, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson);
     })
